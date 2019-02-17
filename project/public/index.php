8d2c953c9e292ab452b8716a21abc845e66e29cc
Ioannis Monios <imonios@forawalk.gr>
2019-02-17 11:54:06 +0200

    Modification of index

--- project/public/index.php
+++ project/public/index.php
@@ -1,7 +1,25 @@
 <?php
 
-use App\Example;
+use App\Model\Instructions;
+use App\RoverController;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-new Example();
+$transmission = <<<input
+5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM
+input;
+
+$instructions = new Instructions($transmission);
+$roverController = new RoverController($instructions);
+$numberOfRovers = $roverController->getNumberOfRovers();
+$roversStateHistories = [];
+for ($i=0; $i < $numberOfRovers; $i++) {
+    $roverStateHistory = $roverController->instructRover($i);
+    $lastRoverState = $roverStateHistory->getLastState();
+    $serializedState = $lastRoverState->serializeState();
+    echo $serializedState . "\n";
+}
